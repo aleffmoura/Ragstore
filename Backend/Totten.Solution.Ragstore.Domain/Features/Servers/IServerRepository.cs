80bfada3c9b79b1e0040b09fa773e958096b1a0b
Aleff Moura <aleff_moura@hotmail.com>
2024-04-29 09:14:16 -0300

    adicionado verify em server name

--- Backend/Totten.Solution.Ragstore.Domain/Features/Servers/IServerRepository.cs
+++ Backend/Totten.Solution.Ragstore.Domain/Features/Servers/IServerRepository.cs
@@ -9,6 +9,9 @@ public interface IServerRepository : IRepository<Server, int>
     {
         try
         {
+            if (serverName is null or "")
+                return new InvalidObjectError("ServerName não fo informado.");
+
             var server = this.GetAllByFilter(x => serverName.Equals(x.Name)).FirstOrDefault();
 
             return server is null ? new NotFoundError("Servidor não encontrado") : server;
